f7ddfcb111bb4d04d580d9ce08cc8dae97d80d86
Mattia Braga <mattiabraga2006@gmail.com>
2024-12-01 19:41:23 +0100

    debug

--- bin/qrcode_val.py
+++ bin/qrcode_val.py
@@ -92,6 +92,7 @@ async def generate_qr_codes(password: str):
 
 @app.get("/send_email")
 async def send_email(password: str):
+    print(password)
     if password != PASSWORD:
         return {"status": "invalid password"}
     email_sender()
