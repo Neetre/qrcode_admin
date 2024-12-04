bf2632dffd79cc3051f0b0dac77cd5d5c3f518e7
Mattia Braga <mattiabraga2006@gmail.com>
2024-12-04 21:25:53 +0100

    pippo

--- bin/qrcode_val.py
+++ bin/qrcode_val.py
@@ -94,4 +94,4 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     DEFAUTL_LINK = f"http://{args.ip_address if not args.domain else args.domain}:{args.port}/qr_code?code="
-    uvicorn.run(app, host="127.0.0.1", port="5000")
+    uvicorn.run(app, port="5000")
