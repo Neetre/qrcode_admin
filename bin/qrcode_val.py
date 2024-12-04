2a62366e11b478f579ac471796cbe50a88aece80
Mattia Braga <mattiabraga2006@gmail.com>
2024-12-04 21:05:33 +0100

    cloudflare binding error

--- bin/qrcode_val.py
+++ bin/qrcode_val.py
@@ -94,4 +94,4 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     DEFAUTL_LINK = f"http://{args.ip_address if not args.domain else args.domain}:{args.port}/qr_code?code="
-    uvicorn.run(app, host=args.ip_address if not args.domain else args.domain, port=args.port)
+    uvicorn.run(app, host=args.ip_address, port=args.port)
