896d319b02aa8027dc7536177021be14ac2c790e
Mattia Braga <mattiabraga2006@gmail.com>
2024-12-04 20:50:49 +0100

    updated get system

--- bin/qrcode_val.py
+++ bin/qrcode_val.py
@@ -36,47 +36,26 @@ def generate_qr_code():
 
 # API section
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
 )
 
-"""
+
 @app.get("/qr_code", response_class=HTMLResponse)
-async def get_qr_code_page(request: Request):
-    return templates.TemplateResponse("qr_code_form.html", {"request": request})
+async def validate_qr_code(request: Request):
+    code = request.query_params.get("code")
+    qr_code = datamanager.get_single_code(code)
+    if qr_code is None:
+        return {"status": "invalid"}
 
-@app.post("/qr_code")
-async def post_qr_code_page(code: str = Form(...), password: str = Form(...)):
-    if password != PASSWORD:
-        return {"status": "invalid password"}
-
-    code = datamanager.get_single_code(code)
-    if code is None:
-        return {"status": "not found"}
-
-    if code[2] == 0:
-        datamanager.cursor.execute('''UPDATE codes SET used = 1 WHERE data = ?''', (code[1],))
+    if qr_code[2] == 0:
+        datamanager.cursor.execute('''UPDATE codes SET used = 1 WHERE data = ?''', (code,))
         datamanager.conn.commit()
         return {"status": "valid"}
     else:
         return {"status": "invalid"}
-"""
 
-@app.get("/qr_code")
-async def validate_qr_code(data: str):
-    code = datamanager.get_single_code(data)
-    if code is None:
-        return {"status": "invalid"}
-
-    if code[2] == 0:
-        datamanager.cursor.execute('''UPDATE codes SET used = 1 WHERE data = ?''', (data,))
-        datamanager.conn.commit()
-        return {"status": "valid"}
-    else:
-        return {"status": "invalid"}
 
 @app.get("/admin_login")
 async def admin_login(data: str):
@@ -85,12 +64,14 @@ async def admin_login(data: str):
     else:
         return {"status": "invalid"}
 
+
 @app.get("/generate_qr_codes")
 async def generate_qr_codes(password: str):
     if password != PASSWORD:
         return {"status": "invalid password"}
     generate_qr_code()
     return {"status": "ok"}
+
 
 @app.get("/send_email")
 async def send_email(password: str):
@@ -111,4 +92,4 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     DEFAUTL_LINK = f"http://{args.ip_address if not args.domain else args.domain}:{args.port}/qr_code?code="
-    uvicorn.run(app, host=args.ip_address, port=args.port)
+    uvicorn.run(app, host=args.ip_address if not args.domain else args.domain, port=args.port)
